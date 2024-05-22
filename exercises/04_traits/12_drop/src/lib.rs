22766ba0040c3f3896dce4943afd92dd020c50df
skoocda <skoocda@protonmail.com>
2024-05-22 13:54:57 -0700

    Section 4 complete

--- exercises/04_traits/12_drop/src/lib.rs
+++ exercises/04_traits/12_drop/src/lib.rs
@@ -2,6 +2,35 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+struct DropBomb {
+    red_wire: bool,
+    green_wire: bool,
+    blue_wire: bool
+}
+
+impl DropBomb {
+    fn new() -> Self {
+        DropBomb {
+            red_wire: true,
+            green_wire: true,
+            blue_wire: true
+        }
+    }
+    fn defuse(&mut self) {
+        self.red_wire = false;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if self.red_wire == true {
+            panic!("BOOM");
+        } else {
+            println!("Bomb has been defused. Counter-terrorists win")
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
